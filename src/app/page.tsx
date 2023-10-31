45f6db4f268d80b8fdcd74036075b39fd68c9ab2
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-10-31 10:52:10 +0600

    added basic pdf to text feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import Logo from "@/components/logo/Logo";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ImAttachment } from "react-icons/im";
 
 export default function Home() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [extractedText, setExtractedText] = useState("");
+
+  const handleFileChange = (event: any) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileSelect = () => {
+    document.getElementById("fileInput")?.click();
+  };
+
+  const handleSubmit = async () => {
+    if (!selectedFile) {
+      // Handle case where no file is selected
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("pdf_file", selectedFile);
+
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/cv/extract-pdf-text/",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      const data = await response.json();
+      setExtractedText(data.extracted_text);
+      // console.log(data.extracted_text); // Log the response data
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   const tags = Array.from({ length: 50 }).map(
     (_, i, a) => `v1.2.0-beta.${a.length - i}`
   );
@@ -27,23 +67,30 @@ export default function Home() {
             <h4 className="inline-block px-2 mb-4 font-medium leading-none border-b-2 pb-3">
               Extracted Texts
             </h4>
-            {tags.map((tag) => (
-              <div key={tag} className="text-sm">
-                {tag}
-              </div>
-            ))}
+            <div className="text-sm">{extractedText}</div>
           </div>
         </ScrollArea>
       </div>
 
       <div className="w-full flex gap-3 justify-center items-center mb-32 text-center lg:mb-0 lg:text-left">
-        <ImAttachment className="h-6 w-6 text-gray-500 cursor-pointer hover:text-gray-600 transition duration-300" />
+        <input
+          type="file"
+          id="fileInput"
+          onChange={handleFileChange}
+          style={{ display: "none" }}
+        />
+        <ImAttachment
+          onClick={handleFileSelect}
+          className="h-6 w-6 text-gray-500 cursor-pointer hover:text-gray-600 transition duration-300"
+        />
         <input
           type="text"
           placeholder="Write Something..."
           className="w-full h-12 px-4 py-2 text-sm border rounded-md lg:px-6 lg:py-3 focus:outline-none focus:border-gray-400 transition duration-300"
         />
-        <Button className="h-12">Send</Button>
+        <Button onClick={handleSubmit} className="h-12">
+          Send
+        </Button>
       </div>
     </main>
   );
