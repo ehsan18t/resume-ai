3a011954ee484069d3b2104948a1c8226274228f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 22:35:22 +0600

    fixed missing toast messages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} min-w-full min-h-screen`}>
         {children}
+        <ToastContainer />
       </body>
     </html>
   );
