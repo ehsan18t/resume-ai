9a6ebd6914feb46e0d9474f6b6566c6144a56e9f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 16:21:10 +0600

    added Project card

---
+++ src/components/profiles/Projects.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+
+export default function Projects({ projects, className }) {
+  return (
+    <div className={cn("mb-8", className)}>
+      <h2 className="text-2xl font-bold mb-2">Projects</h2>
+      <ul className="list-disc list-inside mb-3">
+        {projects?.map((project, index) => (
+          <li
+            key={index}
+            className="mb-2 list-none border-b-2 border-dashed pb-2"
+          >
+            <strong>{project.name}:</strong> {project.description}
+            <ul className="list-none py-2 flex gap-2">
+              {project.tags.map((tag, idx) => (
+                <li
+                  key={idx}
+                  className="bg-gray-200 py-1 px-2 rounded-full text-sm"
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/components/profiles/index.ts
+++ src/components/profiles/index.ts
@@ -2,4 +2,5 @@ export { default as Contact } from "./Contact";
 export { default as Education } from "./Education";
 export { default as Header } from "./Header";
 export { default as Languages } from "./Languages";
+export { default as Projects } from "./Projects";
 export { default as Skills } from "./Skills";
