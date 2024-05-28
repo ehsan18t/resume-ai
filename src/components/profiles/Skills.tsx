1369852750375d5a710c9f77df645505c18a0de9
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 16:09:40 +0600

    fixed bullet point padding issue

--- src/components/profiles/Skills.tsx
+++ src/components/profiles/Skills.tsx
@@ -4,7 +4,7 @@ const Skills = ({ skills, className }) => {
   return (
     <div className={cn(className)}>
       <h2 className="text-2xl font-bold pb-1">Skills</h2>
-      <ul className="flex flex-wrap justify-between flex-row text-justify">
+      <ul className="flex flex-wrap justify-between flex-row text-justify pl-1">
         {skills.map((skill, index) => (
           <li className="text-lg text-gray-700 list-disc mx-3" key={index}>
             {skill.name}
