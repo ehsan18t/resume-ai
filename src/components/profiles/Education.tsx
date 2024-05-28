4ebe71241f4dd3eb392653e98d1ce4ec770286ef
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 14:51:55 +0600

    added Education card

---
+++ src/components/profiles/Education.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+
+const Education = ({ educations, className }) => {
+  return (
+    <div className={cn("mb-8", className)}>
+      <h2 className="text-2xl font-bold pb-4">Education</h2>
+      {educations &&
+        educations.map((edu, index) => (
+          <div key={index} className="mb-4">
+            <h3 className="text-xl font-semibold">{edu.institution}</h3>
+            <div className="flex gap-3 justify-between items-center text-start">
+              <p>{edu.degree}</p>
+              <p className="text-sm">
+                ({edu.start_date} -{" "}
+                {edu.end_date == null || !edu.end_date
+                  ? edu.end_date
+                  : "Present"}
+                )
+              </p>
+            </div>
+            <p>
+              <strong>CGPA:</strong> {edu.cgpa}
+            </p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Education;
---
+++ src/components/profiles/index.ts
@@ -0,0 +1,3 @@
+export { default as Contact } from "./Contact";
+export { default as Education } from "./Education";
+export { default as Header } from "./Header";
