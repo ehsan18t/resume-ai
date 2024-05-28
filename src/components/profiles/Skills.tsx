35791a6da13dfa9aa99af95f97a545f887b3bc63
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 14:59:13 +0600

    added skills components

---
+++ src/components/profiles/Skills.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+
+const Skills = ({ skills, className }) => {
+  return (
+    <div className={cn(className)}>
+      <h2 className="text-2xl font-bold pb-1">Skills</h2>
+      <ul className="flex flex-wrap justify-between flex-row text-justify">
+        {skills.map((skill, index) => (
+          <li className="text-lg text-gray-700 list-disc mx-3" key={index}>
+            {skill.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
--- src/components/profiles/index.ts
+++ src/components/profiles/index.ts
@@ -1,3 +1,4 @@
 export { default as Contact } from "./Contact";
 export { default as Education } from "./Education";
 export { default as Header } from "./Header";
+export { default as Skills } from "./Skills";
