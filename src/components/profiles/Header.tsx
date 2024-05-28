1e78de6256aef6862c650b44af61db2f6a2c98c1
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 17:10:02 +0600

    made bio optional

--- src/components/profiles/Header.tsx
+++ src/components/profiles/Header.tsx
@@ -12,7 +12,11 @@ const Header = ({ profile_picture, name, title = null, bio, className }) => {
       <div className="flex flex-col">
         <h1 className="text-4xl font-bold">{name}</h1>
         {title && <h2 className="text-xl pb-1 text-stone-600">{title}</h2>}
-        <p className="text-sm font-semibold text-justify">{bio}</p>
+        {bio && (
+          <>
+            <p className="text-gray-600 text-justify">{bio}</p>
+          </>
+        )}
       </div>
     </div>
   );
