13a40d35c2e73503dc97e1907306121c76fd4c5e
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-28 13:59:14 +0600

    added header component

---
+++ src/components/profiles/Header.tsx
@@ -0,0 +1,23 @@
+import Image from "@/components/common/Image";
+
+const Header = ({ profile_picture, name, title = null, bio }) => {
+  return (
+    <div className="w-full flex gap-5 items-center">
+      <Image
+        src={profile_picture}
+        alt={name}
+        className="border-gray-400"
+        height={120}
+        width={120}
+        rounded
+      />
+      <div className="flex flex-col">
+        <h1 className="text-4xl font-bold">{name}</h1>
+        {title && <h2 className="text-xl pb-1 text-stone-600">{title}</h2>}
+        <p className="text-sm font-semibold text-justify">{bio}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
