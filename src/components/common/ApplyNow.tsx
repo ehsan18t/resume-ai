eec74265b5f69fc7f476855400e79c4f2e929d3f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-29 12:23:00 +0600

    disable button after apply

--- src/components/common/ApplyNow.tsx
+++ src/components/common/ApplyNow.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { useApplyJobMutation } from "@/redux/features/cvApiSlice";
 
 export default function ApplyNow({ post }) {
-  const [applyJob, { isLoading }] = useApplyJobMutation();
+  const [applyJob, { isLoading, isSuccess }] = useApplyJobMutation();
 
   const handleApply = async () => {
     await applyJob({ post: post.id });
@@ -15,7 +15,7 @@ export default function ApplyNow({ post }) {
       className="bg-slate-200 hover:bg-slate-300"
       size="sm"
       onClick={handleApply}
-      disabled={isLoading}
+      disabled={isLoading || isSuccess || post.is_applied}
     >
       {isLoading ? "Applying..." : "Apply Now"}
     </Button>
