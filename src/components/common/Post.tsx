b965669dbe982a83d67e5020993e6922eb2f0a81
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-29 11:09:54 +0600

    added apply now button in job post

--- src/components/common/Post.tsx
+++ src/components/common/Post.tsx
@@ -14,7 +14,7 @@ import { AiOutlineLike } from "react-icons/ai";
 import { BiComment } from "react-icons/bi";
 import { MdOutlineBookmarkAdd } from "react-icons/md";
 
-function Post({ user, content, time }) {
+function Post({ user, post }) {
   return (
     <Card className="my-4">
       <CardHeader>
@@ -23,21 +23,34 @@ function Post({ user, content, time }) {
             {user.first_name} {user.first_name}
           </Link>
         </CardTitle>
-        <CardDescription>{time}</CardDescription>
+        <CardDescription>{post.formatted_time}</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>{content}</p>
+        <p>{post.content}</p>
       </CardContent>
-      <CardFooter className="flex gap-3">
-        <Button variant="outline" size="icon">
-          <AiOutlineLike size={25} />
-        </Button>
-        <Button variant="outline" size="icon">
-          <BiComment size={25} />
-        </Button>
-        <Button variant="outline" size="icon">
-          <MdOutlineBookmarkAdd size={25} />
-        </Button>
+      <CardFooter className="flex justify-between">
+        <div className="flex gap-3">
+          <Button variant="outline" size="icon">
+            <AiOutlineLike size={25} />
+          </Button>
+          <Button variant="outline" size="icon">
+            <BiComment size={25} />
+          </Button>
+          <Button variant="outline" size="icon">
+            <MdOutlineBookmarkAdd size={25} />
+          </Button>
+        </div>
+        <div>
+          {post.is_job_circular && (
+            <Button
+              variant="ghost"
+              className="bg-slate-200 hover:bg-slate-300"
+              size="sm"
+            >
+              Apply Now
+            </Button>
+          )}
+        </div>
       </CardFooter>
     </Card>
   );
