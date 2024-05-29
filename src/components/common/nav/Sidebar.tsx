509f4a69e399f0da1cb5112db32d7c14a8c7ba81
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-29 06:01:54 +0600

    added settings to the sidebar

--- src/components/common/nav/Sidebar.tsx
+++ src/components/common/nav/Sidebar.tsx
@@ -1,7 +1,7 @@
+import { BsGear } from "react-icons/bs";
 import { CiLogout, CiUser } from "react-icons/ci";
 import { NavItem } from "./NavItem";
 import SidebarHeader from "./SidebarHeader";
-
 export default function Sidebar({
   user,
   onClose,
@@ -11,7 +11,7 @@ export default function Sidebar({
 }) {
   return (
     <div
-      className={`z-[2000] shadow-md fixed top-0 right-0 h-full w-96 bg-white text-gray-800 transform transition-transform ease-in-out duration-300 ${
+      className={`z-[99999999] shadow-md fixed top-0 right-0 h-full w-96 bg-white text-gray-800 transform transition-transform ease-in-out duration-300 ${
         isSidebarOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
@@ -24,6 +24,7 @@ export default function Sidebar({
             name="Profile"
             icon={CiUser}
           />
+          <NavItem to={`/settings`} name="Settings" icon={BsGear} />
           <NavItem onClick={onLogout} name="Logout" icon={CiLogout} />
           {children}
         </div>
