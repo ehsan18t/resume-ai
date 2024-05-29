4ad0fe04f30a5e5662ab02660db44027bbd65c84
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-29 09:07:36 +0600

    created api slice for post

---
+++ src/redux/features/postApiSlice.ts
@@ -0,0 +1,41 @@
+import { apiSlice } from "../services/apiSlice";
+
+const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    retrieveUserPosts: builder.query({
+      query: () => "/post/user-posts/",
+    }),
+    retrieveCategoryPosts: builder.query({
+      query: (slung) => `/post/category-posts/?slung=${slung}`,
+    }),
+    retrieveCategoryList: builder.query({
+      query: () => "/post/category-list/",
+    }),
+    retrieveFeed: builder.query({
+      query: () => "/post/feed/",
+    }),
+    createCategory: builder.mutation({
+      query: ({ category }) => ({
+        url: "/post/category/create/",
+        method: "POST",
+        body: { category },
+      }),
+    }),
+    createPost: builder.mutation({
+      query: ({ content, is_job_circular, category }) => ({
+        url: "/post/create/",
+        method: "POST",
+        body: { content, is_job_circular, category },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useRetrieveUserPostsQuery,
+  useRetrieveCategoryPostsQuery,
+  useRetrieveCategoryListQuery,
+  useRetrieveFeedQuery,
+  useCreateCategoryMutation,
+  useCreatePostMutation,
+} = authApiSlice;
