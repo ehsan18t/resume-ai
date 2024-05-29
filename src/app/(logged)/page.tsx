f68f87075a0961de2f920e9cacf2e7070c20a064
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-29 09:30:18 +0600

    completed feed post

--- src/app/(logged)/page.tsx
+++ src/app/(logged)/page.tsx
@@ -1,73 +1,15 @@
+"use client";
+
 import Post from "@/components/common/Post";
 import NewPost from "@/components/page-specific/NewPost";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { useRetrieveFeedQuery } from "@/redux/features/postApiSlice";
 
 export default function Feed() {
-  const posts = [
-    {
-      id: 1,
-      user: {
-        firstName: "John",
-        lastName: "Doe",
-      },
-      content: "This is my first post!",
-      time: "2 hours ago",
-    },
-    {
-      id: 2,
-      user: {
-        firstName: "Jane",
-        lastName: "Doe",
-      },
-      content: "Hello, world!",
-      time: "3 hours ago",
-    },
-    {
-      id: 2,
-      user: {
-        firstName: "Jane",
-        lastName: "Doe",
-      },
-      content: "Hello, world!",
-      time: "3 hours ago",
-    },
-    {
-      id: 2,
-      user: {
-        firstName: "Jane",
-        lastName: "Doe",
-      },
-      content: "Hello, world!",
-      time: "3 hours ago",
-    },
-    {
-      id: 2,
-      user: {
-        firstName: "Jane",
-        lastName: "Doe",
-      },
-      content: "Hello, world!",
-      time: "3 hours ago",
-    },
-    {
-      id: 2,
-      user: {
-        firstName: "Jane",
-        lastName: "Doe",
-      },
-      content: "Hello, world!",
-      time: "3 hours ago",
-    },
-    {
-      id: 2,
-      user: {
-        firstName: "Jane",
-        lastName: "Doe",
-      },
-      content: "Hello, world!",
-      time: "3 hours ago",
-    },
-  ];
+  let { data: posts, isLoading } = useRetrieveFeedQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  posts = posts?.posts;
 
   return (
     <div className="w-full h-full mx-auto">
@@ -77,10 +19,10 @@ export default function Feed() {
       <ScrollArea className="mx-auto h-[550px] w-3/5 py-1 p-4 rounded-lg">
         {posts.map((post) => (
           <Post
-            key={post.id}
+            key={`post_${post.id}`}
             user={post.user}
             content={post.content}
-            time={post.time}
+            time={post.formatted_time}
           />
         ))}
       </ScrollArea>
--- src/components/common/Post.tsx
+++ src/components/common/Post.tsx
@@ -7,16 +7,21 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import { AiOutlineLike } from "react-icons/ai";
 import { BiComment } from "react-icons/bi";
 import { MdOutlineBookmarkAdd } from "react-icons/md";
+
 function Post({ user, content, time }) {
   return (
     <Card className="my-4">
       <CardHeader>
         <CardTitle className="text-lg">
-          {user.firstName} {user.lastName}
+          <Link href={`/profile/${user?.username}`}>
+            {user.first_name} {user.first_name}
+          </Link>
         </CardTitle>
         <CardDescription>{time}</CardDescription>
       </CardHeader>
