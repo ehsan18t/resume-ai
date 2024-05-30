6c82d2dbca35063b9f8b0ac2be1ae9d7fddd88f5
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-30 21:37:25 +0600

    updated initial loading & verifications

--- src/components/utils/Setup.tsx
+++ src/components/utils/Setup.tsx
@@ -3,7 +3,10 @@
 import { useVerify } from "@/hooks";
 
 export default function Setup() {
-  useVerify();
+  const { isLoading, isError } = useVerify();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error during verification</p>;
 
   return <></>;
 }
--- src/hooks/use-verify.ts
+++ src/hooks/use-verify.ts
@@ -1,21 +1,40 @@
-import { useVerifyMutation } from "@/redux/features/authApiSlice";
-import { finishInitialLoad, setAuth } from "@/redux/features/authSlice";
+import {
+  useRetrieveUserQuery,
+  useVerifyMutation,
+} from "@/redux/features/authApiSlice";
+import {
+  finishInitialLoad,
+  setAuth,
+  setUserData,
+} from "@/redux/features/authSlice";
 import { useAppDispatch } from "@/redux/hooks";
 import { useEffect } from "react";
 
 export default function useVerify() {
   const dispatch = useAppDispatch();
+  const { data: user, isLoading: isUserLoading } = useRetrieveUserQuery();
 
-  const [verify] = useVerifyMutation();
+  const [verify, { isLoading: isVerifyLoading, isError }] = useVerifyMutation();
 
   useEffect(() => {
-    verify(undefined)
+    verify()
       .unwrap()
       .then(() => {
         dispatch(setAuth());
+        if (user) {
+          dispatch(setUserData(user));
+        }
+      })
+      .catch((error) => {
+        console.error("Verification failed", error);
       })
       .finally(() => {
         dispatch(finishInitialLoad());
       });
-  }, []);
+  }, [verify, dispatch, user]);
+
+  return {
+    isLoading: isVerifyLoading || isUserLoading,
+    isError,
+  };
 }
