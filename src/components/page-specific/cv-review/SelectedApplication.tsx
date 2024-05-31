667e9554616284af3ea9a1e3e986dc9a548b6853
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-31 13:17:35 +0600

    fetch necessary info for selected candidate

--- src/components/page-specific/cv-review/SelectedApplication.tsx
+++ src/components/page-specific/cv-review/SelectedApplication.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { useRetrieveProfileSummaryQuery } from "@/redux/features/cvApiSlice";
+import { useEffect, useState } from "react";
+
 export default function SelectedApplication({ application, className }) {
+  const { data, isLoading } = useRetrieveProfileSummaryQuery(
+    application?.user.id
+  );
+
+  const [profile, setProfile] = useState(null);
+  const [summary, setSummary] = useState(null);
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      setProfile(data.profile);
+      setSummary(data.summary.summary);
+    }
+  }, [data, isLoading]);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  console.log(profile, summary);
+
   return (
     <div className={className}>
       {application ? (
         <div>
           <h1 className="text-2xl py-2 font-bold">Profile</h1>
+          {profile?.title}
         </div>
       ) : (
         <p>Select an application</p>
