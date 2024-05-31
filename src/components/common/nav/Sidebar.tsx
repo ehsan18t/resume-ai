6045a8bc2c3316ed243257047cf25f8805e7773e
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-31 11:28:07 +0600

    added template page for applied jobs

--- src/components/common/nav/Sidebar.tsx
+++ src/components/common/nav/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { BsGear } from "react-icons/bs";
 import { CiLogout, CiUser } from "react-icons/ci";
+import { IoBriefcaseOutline } from "react-icons/io5";
 import { NavItem } from "./NavItem";
 import SidebarHeader from "./SidebarHeader";
 export default function Sidebar({
@@ -23,6 +24,11 @@ export default function Sidebar({
             to={`/profile/${user?.username}`}
             name="Profile"
             icon={CiUser}
+          />
+          <NavItem
+            to={`/cv/applied`}
+            name="Applied Jobs"
+            icon={IoBriefcaseOutline}
           />
           <NavItem to={`/settings`} name="Settings" icon={BsGear} />
           <NavItem onClick={onLogout} name="Logout" icon={CiLogout} />
