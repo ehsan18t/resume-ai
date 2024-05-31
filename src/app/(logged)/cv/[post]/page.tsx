c432f8f60b11b07e1c17006a43fde6dc09efcb90
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-05-31 13:17:46 +0600

    updated cv screening layout

--- src/app/(logged)/cv/[post]/page.tsx
+++ src/app/(logged)/cv/[post]/page.tsx
@@ -10,13 +10,13 @@ export default function Page({ params }) {
   const [application, setApplication] = useState(null);
 
   return (
-    <div className="h-screen grid grid-cols-3 gap-2 mx-4">
+    <div className="h-screen grid grid-cols-4 gap-2 mx-4">
       <ApplicationList
         postId={params.post}
         handleSelectedApplication={setApplication}
         className="w-full col-span-1 p-4 border-2 text-center rounded"
       />
-      <div className="w-full col-span-2 border-2 p-4 text-center rounded">
+      <div className="w-full col-span-3 border-2 p-4 text-center rounded">
         {application ? (
           <SelectedApplication application={application} />
         ) : (
