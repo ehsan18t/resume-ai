00607bc8725441f11a8e4c42e9a8e922c68cde57
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-02 15:27:19 +0600

    updated post buttons

--- src/components/common/ApplyNow.tsx
+++ src/components/common/ApplyNow.tsx
@@ -2,7 +2,8 @@
 import { Button } from "@/components/ui/button";
 import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
 import { useApplyJobMutation } from "@/redux/features/cvApiSlice";
-
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 export default function ApplyNow({ user, post }) {
   const [applyJob, { isLoading, isSuccess }] = useApplyJobMutation();
   const { data: currentUser } = useRetrieveUserQuery();
@@ -17,29 +18,40 @@ export default function ApplyNow({ user, post }) {
 
   return (
     <div className="flex gap-2">
-      <Button
-        variant="ghost"
-        className="bg-slate-200 hover:bg-slate-300"
-        size="sm"
-        onClick={handleApply}
-        disabled={
-          isLoading ||
-          isSuccess ||
-          post.is_applied ||
-          user.id === currentUser?.id
-        }
-      >
-        {isLoading ? "Applying..." : "Apply Now"}
-      </Button>
-      {user.id === currentUser?.id && (
+      {user.id !== currentUser?.id && (
         <Button
           variant="ghost"
           className="bg-slate-200 hover:bg-slate-300"
           size="sm"
-          onClick={browse}
+          onClick={handleApply}
+          disabled={isLoading || isSuccess || post.is_applied}
         >
-          Check Applications
+          {isLoading ? "Applying..." : "Apply Now"}
         </Button>
+      )}
+
+      {user.id === currentUser?.id && (
+        <div className="flex gap-3">
+          <Button
+            className="bg-destructive hover:bg-destructive/80 text-destructive-foreground  hover:text-destructive-foreground"
+            variant="outline"
+            size="icon"
+          >
+            <MdDeleteOutline size={25} />
+          </Button>
+          <Button variant="outline" size="icon">
+            <CiEdit size={25} />
+          </Button>
+
+          <Button
+            variant="ghost"
+            className="bg-slate-200 hover:bg-slate-300"
+            size="sm"
+            onClick={browse}
+          >
+            Check Applications
+          </Button>
+        </div>
       )}
     </div>
   );
