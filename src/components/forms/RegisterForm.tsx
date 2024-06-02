4969dd70c088373d13f8cf06c037608044beae75
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-02 14:39:17 +0600

    updated some of the components color

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -86,7 +86,7 @@ export default function RegisterForm() {
       <div className="pt-6 w-full">
         <button
           type="submit"
-          className="w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           disabled={isLoading}
         >
           {isLoading ? <Spinner sm /> : "Sign up"}
