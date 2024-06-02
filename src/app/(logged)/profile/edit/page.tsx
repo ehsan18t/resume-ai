efbcb5cb8763a4463ff6905214ab6f9bb6138d95
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-02 18:11:15 +0600

    added initial edit profile page

---
+++ src/app/(logged)/profile/edit/page.tsx
@@ -0,0 +1,10 @@
+import EditProfile from "./EditProfile";
+
+export const metadata = {
+  title: "Edit Profile",
+  description: "Edit Profile",
+};
+
+export default function page() {
+  return <EditProfile />;
+}
